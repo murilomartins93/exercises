2301db405ac2f2e697934bd81699eeefb09e3a8a
Murilo Martins <murilomartins@outlook.com>
2022-04-11 18:06:03 -0300

    :books: Exercícios realizados

---
+++ Java/Exercicio15.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercicio15 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int senha;
+		
+		System.out.print("Digite a senha: ");
+		senha = sc.nextInt();
+		
+		while(senha != 2002) {
+			System.out.print("Senha Inválida. Digite novamente: ");
+			senha = sc.nextInt();
+		}
+		
+		System.out.println("Acesso Permitido!");
+		
+		sc.close();
+	}
+
+}
---
+++ Java/Exercicio17.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Exercicio17 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int cod, alcool, gasolina, diesel;
+
+		System.out.print("Informe o código do combustível abastecido ou 4 para encerrar: ");
+		cod = sc.nextInt();	
+		
+		alcool = 0;
+		gasolina = 0;
+		diesel = 0;
+
+		do {			
+			switch (cod) {
+			case 1:
+				alcool++;
+				break;
+			case 2:
+				gasolina++;
+				break;
+			case 3:
+				diesel++;
+				break;	
+			default: 
+				System.out.println("Código inválido!\n");		
+				break;
+			}
+			System.out.print("Digite um novo código ou 4 para encerrar: ");
+			cod = sc.nextInt();
+		} while (cod != 4);
+
+		System.out.printf("MUITO OBRIGADO!%n");
+		System.out.printf("Alcool: %d%n", alcool);
+		System.out.printf("Gasolina: %d%n", gasolina);
+		System.out.printf("Diesel: %d%n", diesel);
+
+		sc.close();
+
+	}
+
+}
