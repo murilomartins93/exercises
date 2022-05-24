05358eb64d0bbdd597a4b23cd303bd743e4d3556
Murilo Martins <murilomartins@outlook.com>
2022-05-24 16:33:11 -0300

    :books: Exercícios realizados

---
+++ Java/Java03.java
@@ -0,0 +1,14 @@
+class AtividadePratica13 {
+
+   public static void main(String entrada[]) {
+      
+	String name = "Murilo Martins";
+	String course = "Analise e Desenvolvimento de Sistemas";
+	int age = 29;
+	char gender = 'M';
+	double weight = 100.6;  	
+
+	System.out.println("Eu sou o " + name + ", tenho " + age + " anos, sexo " + gender + ", tenho " + weight + "kg de peso e estou cursando " + course + ".");
+	System.exit(0);
+   }
+}
---
+++ Java/Java11.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+
+class Java11
+{
+  public static void main (String entrada[])
+  {
+        
+	int num1, num2, produto = 1;
+        char op = '0';
+        String msg = "", msgEntr = "Digite 1 para produto\nDigite 2 para produtoria\n";
+         
+        num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero inteiro"));
+	num2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro numero inteiro"));
+	op = JOptionPane.showInputDialog(msgEntr).charAt(0);
+
+	switch (op)
+        {
+		case '1':
+                {
+			if (num1 > 0 && num2 > 0) {
+				produto = num1 * num2;
+				msg = msg + "Produto de " + num1 + " por " + num2 + " = " + produto + "\n\n";
+			}
+			else {
+				msg = "Um dos numeros nao eh par\n";
+			}
+		     
+                	break;
+		}
+                case '2':
+			for (int i = 1; i <= num2; i++){
+				produto *= num1;
+			}
+			msg = msg + "Produtoria de " + num1 + ", por " + num2 + " vezes = " + produto + "\n\n";
+			break;
+
+                default: JOptionPane.showMessageDialog(null, "Opcao invalida, calculos nao realizados");
+	}
+  
+	if (op == '1' || op == '2')
+        {
+		JOptionPane.showMessageDialog(null, msg);
+        }
+        System.exit(0);
+  }
+}
