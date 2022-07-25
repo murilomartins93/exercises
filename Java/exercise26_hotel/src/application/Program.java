7ad57d283f75e6a87aa67f083b772c61a84ff252
Murilo Martins <murilomartins@outlook.com>
2022-07-25 15:32:25 -0300

    :books: Exercícios realizados

--- Java/exercise26_hotel/src/application/Program.java
+++ Java/exercise26_hotel/src/application/Program.java
@@ -35,19 +35,15 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates must be future dates");
-			} else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
+			String error = reserve.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
 			} else {
-				reserve.updateDates(checkIn, checkOut);
-				System.out.println("Reservation: " + reserve);
-			}	
-						
-		}
+			System.out.println("Reservation: " + reserve);
+			}
+		}	
 		
 		sc.close();
+
 	}
-	
 }
